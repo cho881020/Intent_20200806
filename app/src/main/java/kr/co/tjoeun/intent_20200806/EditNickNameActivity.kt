9234bf6c9400268040e6af06a4059f2e8c3cd783
조경진 <kj_cho@idealidea.co.kr>
2020-08-06 22:07:41 +0900

    닉네임 변경화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/intent_20200806/EditNickNameActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.intent_20200806
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNickNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nick_name)
+    }
+}
