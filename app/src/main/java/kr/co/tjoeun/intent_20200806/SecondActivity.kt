7cce9ef3d395c663dac1ee603af44b5ffbd7ca06
조경진 <kj_cho@idealidea.co.kr>
2020-08-06 21:38:02 +0900

    SecondActivity 추가 -> Main에서 전달해준 내용 출력 예정

---
+++ app/src/main/java/kr/co/tjoeun/intent_20200806/SecondActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.intent_20200806
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
