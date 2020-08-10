af3b627df51dbe389665b24ab0c637787b52f2ab
조경진 <kj_cho@idealidea.co.kr>
2020-08-10 19:38:44 +0900

    완료 버튼 누르면 => 입력한 닉네임을 들고 finish

--- app/src/main/java/kr/co/tjoeun/intent_20200806/EditNickNameActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200806/EditNickNameActivity.kt
@@ -1,5 +1,7 @@
 package kr.co.tjoeun.intent_20200806
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_nick_name.*
@@ -13,6 +15,16 @@ class EditNickNameActivity : AppCompatActivity() {
 
 //            완료버튼을 눌렀다 => 정말로 데이터를 들고 돌아가겠다 (확정) => 복귀 : finish()
 //            백버튼을 눌러서 종료하는것과 구별해야함.
+
+//            돌아갈때 첨부할 데이터를 들고 있어주는 Intent 별개로 생성
+            val resultIntent = Intent()
+
+//            입력된 닉네임을 resultIntent에 첨부 - putExtra
+            val inputNickName = nickNameEdt.text.toString()
+            resultIntent.putExtra("newNickName", inputNickName)
+
+//            OK버튼을 눌렀다고 명시 + 데이터를 담고 있는 resultIntent를 갖고 복귀하게.
+            setResult(Activity.RESULT_OK, resultIntent)
 
             finish()
 
